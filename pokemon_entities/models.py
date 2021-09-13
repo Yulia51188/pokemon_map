600979b04812a38b32fb251d64208375f6f0e46d
Yulia51188 <sun51188@gmail.com>
2021-09-13 12:45:31 +0300

    Edit mandatory and optional fields settings

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,8 +6,10 @@ class Pokemon(models.Model):
     photo = models.ImageField('Изображение', upload_to='images', blank=True)
     description = models.TextField('Описание', blank=True,
         default='описание скоро появится')
-    title_en = models.CharField('Имя на английском', max_length=50, default='-')
-    title_jp = models.CharField('Имя на японском', max_length=50, default='-')
+    title_en = models.CharField('Имя на английском', max_length=50,
+        blank=True, default='-')
+    title_jp = models.CharField('Имя на японском', max_length=50, blank=True, 
+        default='-')
     previous_evolution = models.ForeignKey('self', blank=True, null=True,
         on_delete=models.SET_NULL, related_name='next_evolutions',
         verbose_name='Из кого эволюционирует')
@@ -23,11 +25,11 @@ class PokemonEntity(models.Model):
     longitude = models.FloatField('Долгота')
     appeared_at = models.DateTimeField('Время появления', null=True)
     disappeared_at = models.DateTimeField('Время исчезновения', null=True)
-    level = models.IntegerField('Уровень', default=0)
-    health = models.IntegerField('Здоровье', default=0)
-    strength = models.IntegerField('Атака', default=0)
-    defence = models.IntegerField('Защита', default=0)
-    stamina = models.IntegerField('Выносливость', default=0)
+    level = models.IntegerField('Уровень', blank=True, null=True)
+    health = models.IntegerField('Здоровье', blank=True, null=True)
+    strength = models.IntegerField('Атака', blank=True, null=True)
+    defence = models.IntegerField('Защита', blank=True, null=True)
+    stamina = models.IntegerField('Выносливость', blank=True, null=True)
 
     def __str__(self):
         return f'{self.pokemon.title} Lev. {self.level}'
